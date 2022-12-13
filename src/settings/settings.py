ab4b7bc734342568edd92637ca68fe0c327e1e91
Teslenko Aleksandr <aleksandr.teslenko@ru.axept.com>
2022-12-13 17:12:50 +0300

    Переименовал переменные для БД, refs#1

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        "NAME": env("POSTGRES_DB"),
-        "USER": env("POSTGRES_USER"),
-        "PASSWORD": env("POSTGRES_PASSWORD"),
-        "HOST": env("POSTGRES_SERVER"),
-        "PORT": env("POSTGRES_PORT")
+        "NAME": env("APP_POSTGRESQL_NAME"),
+        "USER": env("APP_POSTGRESQL_USER"),
+        "PASSWORD": env("APP_POSTGRESQL_PASSWORD"),
+        "HOST": env("APP_POSTGRESQL_HOST"),
+        "PORT": env("APP_POSTGRESQL_PORT")
 
     }
 }

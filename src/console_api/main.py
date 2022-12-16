ad3594f6109078484c4e873671930e890bb5e768
Tof <tof.aliev@ru.axept.com>
2022-12-16 22:16:20 +0600

    Removed dockerfile & unused migrations, refs #5

--- src/console_api/main.py
+++ src/console_api/main.py
@@ -14,6 +14,5 @@ def execute():
     """
     Function apply migrations:
     """
-    # call_command('collectstatic')
     call_command('migrate')
     call_command(subprocess.call(['gunicorn', 'console_api.settings.wsgi:application', '--bind 0.0.0.0:8080', '--workers=4']))

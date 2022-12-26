2953b8bf97a37b2c50ef0b363289e4fe546df57a
Евгений Степанов <evgeniy.stepanov@ru.axept.com>
2022-12-26 17:50:17 +0300

    Add logs about admin

--- src/console_api/main.py
+++ src/console_api/main.py
@@ -16,6 +16,14 @@ def execute():
     Function to start Django app
     """
     logger.info("Start Django app")
+    from console_api.apps.users.models import User
+
+    if User.objects.filter(login="admin").exists():
+        print("Хеш пароля админа:")
+        print(User.objects.get(login="admin").password)
+    else:
+        print("Пользователь admin не создался")
+
     call_command(subprocess.call([
         'gunicorn', '-b 0.0.0.0:8080', 'console_api.settings.wsgi:application', '--workers=4'
     ]))

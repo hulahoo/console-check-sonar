ae37bb3198254022691e8044c3a340d7a893a0ad
Евгений Степанов <evgeniy.stepanov@ru.axept.com>
2023-01-27 17:24:11 +0300

    Add tests for audit_logs/apps.py

---
+++ src/console_api/audit_logs/tests/test_apps.py
@@ -0,0 +1,28 @@
+"""Test apps.py file"""
+
+from django.apps import AppConfig
+from django.test import TestCase
+
+from console_api.audit_logs.apps import AuditLogsConfig
+
+
+class AuditLogsConfigTests(TestCase):
+    """Test AuditLogsConfig class"""
+
+    def test_default_auto_field(self) -> None:
+        """Test default_auto_field field"""
+
+        self.assertEqual(
+            AuditLogsConfig.default_auto_field,
+            "django.db.models.BigAutoField",
+        )
+
+    def test_name(self) -> None:
+        """Test name field"""
+
+        self.assertEqual(AuditLogsConfig.name, "console_api.audit_logs")
+
+    def test_mro(self) -> None:
+        """Test MRO"""
+
+        self.assertIn(AppConfig, AuditLogsConfig.mro())

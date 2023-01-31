7e4ff9ca8de116d3e98ea652beb4970599952bf4
Евгений Степанов <evgeniy.stepanov@ru.axept.com>
2023-01-31 14:28:04 +0300

    Add tests for audit_logs views for pagination

--- src/console_api/audit_logs/tests/constants.py
+++ src/console_api/audit_logs/tests/constants.py
@@ -1,3 +1,5 @@
 """Constants for tests"""
 
 AUDIT_LOGS_URL = "/api/audit-logs"
+
+TEST_SERVER_URL = "http://testserver"
--- src/console_api/audit_logs/tests/test_views.py
+++ src/console_api/audit_logs/tests/test_views.py
@@ -2,17 +2,22 @@
 
 from django.test import TestCase, Client
 
-from console_api.audit_logs.tests.constants import AUDIT_LOGS_URL
 from console_api.audit_logs.models import AuditLogs
+from console_api.audit_logs.tests.constants import (
+    AUDIT_LOGS_URL,
+    TEST_SERVER_URL,
+)
 from console_api.test_utils import get_authorization_token
 
 
-class ListTests(TestCase):
-    """Test list of logs"""
+class ListWithoutParamsTests(TestCase):
+    """Test list of logs without parameters"""
+
+    __OBJECTS_COUNT = 3
 
     @classmethod
     def setUpTestData(cls) -> None:
-        for i in range(3):
+        for i in range(cls.__OBJECTS_COUNT):
             AuditLogs.objects.create(
                 service_name=f"Service name {i}",
                 user_id=i,
@@ -39,11 +44,11 @@ class ListTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
-        self.assertEqual(response.data.get("count"), 3)
+        self.assertEqual(response.data.get("count"), self.__OBJECTS_COUNT)
         self.assertEqual(response.data.get("next", ""), None)
         self.assertEqual(response.data.get("previous", ""), None)
 
-        for i in range(3):
+        for i in range(self.__OBJECTS_COUNT):
             results = response.data.get("results")[i]
 
             self.assertEqual(results.get("service-name"), f"Service name {i}")
@@ -56,4 +61,115 @@ class ListTests(TestCase):
             self.assertEqual(results.get("prev-value"), {"test": i})
             self.assertEqual(results.get("new-value"), {"test": i})
             self.assertEqual(results.get("context"), {"info": f"INFO {i}"})
+
             self.assertNotEqual(results.get("created-at"), None)
+
+
+class PaginationParametersTests(TestCase):
+    """Test page-number and page-size parameters for logs list"""
+
+    __OBJECTS_COUNT = 50
+
+    __DEFAULT_PAGE_SIZE = 25
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        for i in range(cls.__OBJECTS_COUNT):
+            AuditLogs.objects.create(
+                service_name=f"Service name {i}",
+                user_id=i,
+                user_name=f"User name {i}",
+                event_type=f"Event type {i}",
+                object_type=f"Object type {i}",
+                object_name=f"Object name {i}",
+                description=f"Description {i}",
+                prev_value={"test": i},
+                new_value={"test": i},
+                context={"info": f"INFO {i}"},
+            )
+
+        cls.client = Client()
+        cls.token = get_authorization_token(cls.client)
+
+    def test_page_number_1(self) -> None:
+        """Test page-number=1 parameter"""
+
+        response = self.client.get(
+            AUDIT_LOGS_URL,
+            HTTP_AUTHORIZATION=self.token,
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.data.get("count"), self.__OBJECTS_COUNT)
+        self.assertEqual(
+            response.data.get("next", ""),
+            TEST_SERVER_URL + AUDIT_LOGS_URL + "?page-number=2",
+        )
+
+        self.assertEqual(response.data.get("previous", ""), None)
+
+        self.assertEqual(
+            len(response.data.get("results")),
+            self.__DEFAULT_PAGE_SIZE,
+        )
+
+    def test_page_number_2(self) -> None:
+        """Test page-number=2 parameter"""
+
+        response = self.client.get(
+            f"{AUDIT_LOGS_URL}?page-number=2",
+            HTTP_AUTHORIZATION=self.token,
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.data.get("count"), self.__OBJECTS_COUNT)
+        self.assertEqual(response.data.get("next", ""), None)
+
+        self.assertEqual(
+            response.data.get("previous", ""),
+            TEST_SERVER_URL + AUDIT_LOGS_URL,
+        )
+
+        self.assertEqual(
+            len(response.data.get("results")),
+            self.__DEFAULT_PAGE_SIZE,
+        )
+
+    def test_page_size(self) -> None:
+        """Test page-size parameter"""
+
+        response = self.client.get(
+            f"{AUDIT_LOGS_URL}?page-size=2",
+            HTTP_AUTHORIZATION=self.token,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.get("count"), self.__OBJECTS_COUNT)
+        self.assertEqual(
+            response.data.get("next", ""),
+            TEST_SERVER_URL + AUDIT_LOGS_URL + "?page-number=2&page-size=2",
+        )
+        self.assertEqual(response.data.get("previous", ""), None)
+        self.assertEqual(len(response.data.get("results")), 2)
+
+    def test_page_size_2(self) -> None:
+        """Test page-size parameter for page-number=2"""
+
+        response = self.client.get(
+            f"{AUDIT_LOGS_URL}?page-number=2&page-size=2",
+            HTTP_AUTHORIZATION=self.token,
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.get("count"), self.__OBJECTS_COUNT)
+        self.assertEqual(
+            response.data.get("next", ""),
+            TEST_SERVER_URL + AUDIT_LOGS_URL + "?page-number=3&page-size=2",
+        )
+        self.assertEqual(
+            response.data.get("previous", ""),
+            TEST_SERVER_URL + AUDIT_LOGS_URL + "?page-size=2",
+        )
+        self.assertEqual(len(response.data.get("results")), 2)

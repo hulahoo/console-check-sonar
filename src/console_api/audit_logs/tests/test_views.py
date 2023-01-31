2bbbe8b6f500f51008d7bad5a14659de697d3d33
Евгений Степанов <evgeniy.stepanov@ru.axept.com>
2023-01-31 17:17:39 +0300

    Add test for sort_by + update previous tests

--- src/console_api/audit_logs/tests/test_views.py
+++ src/console_api/audit_logs/tests/test_views.py
@@ -8,6 +8,7 @@ from console_api.audit_logs.tests.constants import (
     TEST_SERVER_URL,
 )
 from console_api.test_utils import get_authorization_token
+from console_api.constants import DIFFERENT_VALUES, WRONG_PAGE_SIZE
 
 
 class ListWithoutParamsTests(TestCase):
@@ -173,3 +174,116 @@ class PaginationParametersTests(TestCase):
             TEST_SERVER_URL + AUDIT_LOGS_URL + "?page-size=2",
         )
         self.assertEqual(len(response.data.get("results")), 2)
+
+    def test_wrong_page_number(self) -> None:
+        """Test wrong page-number parameter"""
+
+        for value in DIFFERENT_VALUES:
+            response = self.client.get(
+                f"{AUDIT_LOGS_URL}?page-number={value}",
+                HTTP_AUTHORIZATION=self.token,
+            )
+
+            self.assertEqual(response.status_code, 404)
+            self.assertEqual(response.data.get("detail"), "Invalid page.")
+
+    def test_wrong_page_size(self) -> None:
+        """Test wrong page-size parameter"""
+
+        for value in WRONG_PAGE_SIZE:
+            with self.subTest(f"{value=}"):
+                response = self.client.get(
+                    f"{AUDIT_LOGS_URL}?page-size={value}",
+                    HTTP_AUTHORIZATION=self.token,
+                )
+
+                self.assertEqual(response.status_code, 400)
+                self.assertEqual(
+                    response.data.get("detail"),
+                    "page-size parameter is wrong",
+                )
+
+
+class SortByTests(TestCase):
+    """Test list with sort-by param"""
+
+    __OBJECTS_COUNT = 10
+
+    __SORT_BY_PARAMS = (
+        "id", "-id",
+        "service-name", "-service-name",
+        "user-id", "-user-id",
+        "user-name", "-user-name",
+        "event-type", "-event-type",
+        "object-type", "-object-type",
+        "object-name", "-object-name",
+        "description", "-description",
+        "created-at", "-created-at",
+    )
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        for i in range(cls.__OBJECTS_COUNT):
+            AuditLogs.objects.create(
+                service_name=f"Service name {i}",
+                user_id=i,
+                user_name=f"User name {i}",
+                event_type=f"Event type {i}",
+                object_type=f"Object type {i}",
+                object_name=f"Object name {i}",
+                description=f"Description {i}",
+                prev_value={"test": i},
+                new_value={"test": i},
+                context={"info": f"INFO {i}"},
+            )
+
+        cls.client = Client()
+        cls.token = get_authorization_token(cls.client)
+
+    def test_sort_by_correct_values(self) -> None:
+        """Tests for sort-by parameter with correct values"""
+
+        for param in self.__SORT_BY_PARAMS:
+            with self.subTest(f"{param=}"):
+                response = self.client.get(
+                    f"{AUDIT_LOGS_URL}?sort-by={param}",
+                    HTTP_AUTHORIZATION=self.token,
+                )
+
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(
+                    response.data.get("count"),
+                    self.__OBJECTS_COUNT,
+                )
+
+                results = response.data.get("results")
+
+                self.assertEqual(len(results), self.__OBJECTS_COUNT)
+
+                if param.startswith("-"):
+                    results_values = [res.get(param[1:]) for res in results]
+
+                    self.assertEqual(
+                        sorted(results_values, reverse=True),
+                        results_values,
+                    )
+                else:
+                    results_values = [res.get(param) for res in results]
+
+                    self.assertEqual(sorted(results_values), results_values)
+
+    def test_sort_by_incorrect_values(self) -> None:
+        """Tests for sort-by parameter with incorrect values"""
+
+        for param in DIFFERENT_VALUES:
+            with self.subTest(f"{param=}"):
+                response = self.client.get(
+                    f"{AUDIT_LOGS_URL}?sort-by={param}",
+                    HTTP_AUTHORIZATION=self.token,
+                )
+
+                self.assertEqual(response.status_code, 400)
+                self.assertEqual(
+                    response.data.get("detail"),
+                    "Wrong value for sort-by parameter",
+                )
--- src/console_api/constants.py
+++ src/console_api/constants.py
@@ -1,5 +1,8 @@
 """Constants for the project"""
 
+from keyword import kwlist
+
+
 CREDS_ERROR = "Authentication credentials were not provided."
 SEARCH_QUERY_ERROR = "Missing or empty 'query' param"
 
@@ -7,3 +10,58 @@ ADMIN_PASS_HASH = \
     "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"
 
 ADMIN_LOGIN = "admin"
+
+
+class TestClass:
+    """Empty class for testing"""
+
+
+def test_function():
+    """Empty function for testing"""
+
+KEYWORDS = (
+    'class', 'list', 'tuple', 'set', 'dict', 'arrow', 'str', 'int',
+    'type', 'float',
+) + tuple([str(keyword) for keyword in kwlist])
+
+NOT_STRING_VALUES = (
+    # int, float and bool
+    0, 100_000, -100, 122.12323, True, False,
+
+    # Data structures
+    [1, 2, 3], (1, 2), {'a': 1, 'b': 2}, set([1, 2, 3]), [[1, 2, 3, (1, 2)]],
+
+    # Types
+    int, bool, str, object, bytes, bytearray, tuple, list, dict, set, float,
+
+    # Classes and functions
+    TestClass, test_function, lambda x: x,
+
+    # Others
+    None, complex(1, 2), bin(20), hex(100), range(100),
+)
+
+WRONG_PAGE_SIZE = (
+    # int, float and bool
+    -100, 122.12323, True, False,
+
+    # Data structures
+    [1, 2, 3], (1, 2), {'a': 1, 'b': 2}, set([1, 2, 3]), [[1, 2, 3, (1, 2)]],
+
+    # Types
+    int, bool, str, object, bytes, bytearray, tuple, list, dict, set, float,
+
+    # Classes and functions
+    TestClass, test_function, lambda x: x,
+
+    # Others
+    None, complex(1, 2), bin(20), hex(100), range(100),
+
+    'value   value', '!@@#Q@$Q', 'test'.encode(), 'a', 'b', 'A', 'B', 'def',
+)
+
+DIFFERENT_STRINGS = (
+    'value   value', '!@@#Q@$Q', 'test'.encode(), 'a', 'b', 'A', 'B', 'def'
+)
+
+DIFFERENT_VALUES = DIFFERENT_STRINGS + NOT_STRING_VALUES

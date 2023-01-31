e84eb921c1328bb8cb9f5054d0979c75f110aaca
Евгений Степанов <evgeniy.stepanov@ru.axept.com>
2023-01-31 12:58:11 +0300

    Update urls.py tests

--- src/console_api/audit_logs/tests/test_urls.py
+++ src/console_api/audit_logs/tests/test_urls.py
@@ -3,6 +3,8 @@
 from console_api.audit_logs.tests.constants import AUDIT_LOGS_URL
 from console_api.test_utils import TestURLMixin
 
+from rest_framework.exceptions import ErrorDetail
+
 
 class UrlsTests(TestURLMixin):
     """Test urls"""
@@ -11,4 +13,15 @@ class UrlsTests(TestURLMixin):
         """Test /api/audit-logs path"""
 
         self._test_url(AUDIT_LOGS_URL, 200, is_authorized=True)
-        self._test_url(AUDIT_LOGS_URL, 403, is_authorized=False)
+
+        self._test_url(
+            path=AUDIT_LOGS_URL,
+            expected_status_code=403,
+            is_authorized=False,
+            data={
+                'detail': ErrorDetail(
+                    string='Authentication credentials were not provided.',
+                    code='not_authenticated'
+                )
+            }
+        )

2edcf3504a70b0f53fb2b07ccafcb6e8d7ac3f12
Евгений Степанов <evgeniy.stepanov@ru.axept.com>
2023-02-02 15:09:42 +0300

    Fix bugs by audit_logs tests

--- src/console_api/audit_logs/views.py
+++ src/console_api/audit_logs/views.py
@@ -1,6 +1,6 @@
 """Views for audit_logs app"""
 
-from rest_framework import generics
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -16,38 +16,47 @@ from console_api.services import (
 )
 
 
-class AuditLogsListView(generics.ListAPIView):
+class AuditLogsListView(ListAPIView):
     """Audit logs list"""
 
     __SORT_BY_PARAMS = (
-        "id", "-id",
-        "service_name", "-service_name",
-        "user_id", "-user_id",
-        "user_name", "-user_name",
-        "event_type", "-event_type",
-        "object_type", "-object_type",
-        "object_name", "-object_name",
-        "description", "-description",
-        "created_at", "-created_at",
+        "id",
+        "-id",
+        "service_name",
+        "-service_name",
+        "user_id",
+        "-user_id",
+        "user_name",
+        "-user_name",
+        "event_type",
+        "-event_type",
+        "object_type",
+        "-object_type",
+        "object_name",
+        "-object_name",
+        "description",
+        "-description",
+        "created_at",
+        "-created_at",
     )
-
-    serializer_class = AuditLogsListSerializer
-    queryset = AuditLogs.objects.all()
 
     authentication_classes = [CustomTokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    queryset = AuditLogs.objects.all()
+    serializer_class = AuditLogsListSerializer
+
     def __filter_queryset(self, request: Request) -> None:
         """Filter the queryset"""
-
-        if created_at := get_filter_query_param(request, "created-at"):
-            self.queryset = self.queryset.filter(created_at=created_at)
 
         if event_id := get_filter_query_param(request, "id"):
             self.queryset = self.queryset.filter(id=event_id)
 
         if service_name := get_filter_query_param(request, "service-name"):
             self.queryset = self.queryset.filter(service_name=service_name)
+
+        if user_id := get_filter_query_param(request, "user-id"):
+            self.queryset = self.queryset.filter(user_id=user_id)
 
         if user_name := get_filter_query_param(request, "user-name"):
             self.queryset = self.queryset.filter(user_name=user_name)
@@ -64,19 +73,25 @@ class AuditLogsListView(generics.ListAPIView):
         if description := get_filter_query_param(request, "description"):
             self.queryset = self.queryset.filter(description=description)
 
+        if created_at := get_filter_query_param(request, "created-at"):
+            self.queryset = self.queryset.filter(created_at=created_at)
+
     def list(self, request: Request, *args, **kwargs) -> Response:
         """Return response with list of logs"""
 
-        self.__filter_queryset(request)
+        try:
+            self.__filter_queryset(request)
 
-        if sort_by := get_sort_by_param(request):
-            if sort_by not in self.__SORT_BY_PARAMS:
-                return Response(
-                    {"detail": "Wrong value for sort-by parameter"},
-                    status=HTTP_400_BAD_REQUEST,
-                )
+            if sort_by := get_sort_by_param(request):
+                if sort_by not in self.__SORT_BY_PARAMS:
+                    return Response(
+                        {"detail": "Wrong value for sort-by parameter"},
+                        status=HTTP_400_BAD_REQUEST,
+                    )
 
-            self.queryset = self.queryset.order_by(sort_by)
+                self.queryset = self.queryset.order_by(sort_by)
+        except Exception as error:
+            return Response({"detail": str(error)}, status=HTTP_400_BAD_REQUEST)
 
         return get_response_with_pagination(
             request,

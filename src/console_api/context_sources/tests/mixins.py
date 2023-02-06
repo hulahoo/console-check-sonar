9c50c59cbce194bcaacc9eef18a5b2e771827659
Евгений Степанов <evgeniy.stepanov@ru.axept.com>
2023-02-06 14:05:44 +0300

    Add tests for context_sources/views.py

---
+++ src/console_api/context_sources/tests/mixins.py
@@ -0,0 +1,32 @@
+"""Mixins for tests"""
+
+from django.test import TestCase, Client
+
+from console_api.context_sources.models import ContextSources
+from console_api.test_utils import get_authorization_token
+
+
+class ContextSourcesListViewTestsMixin(TestCase):
+    """Mixin for ContextSourcesListView tests"""
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        for i in range(cls._CONTEXT_SOURCES_COUNT):
+            ContextSources.objects.create(
+                ioc_type="ip",
+                source_url="https://virustotal.com/api/get_ip_info?ip={value}",
+                request_method=f"get {i}",
+                request_headers="Authorization: Bearer MDM0MjM5NDc2MD",
+                request_body="test",
+                inbound_removable_prefix="data",
+                outbound_appendable_prefix="virustotal",
+                created_by=i,
+            )
+
+        cls.client = Client()
+        cls.token = get_authorization_token(cls.client)
+
+    def get_auth_get_response(self, url: str):
+        """Return authorized GET response"""
+
+        return self.client.get(url, HTTP_AUTHORIZATION=self.token)

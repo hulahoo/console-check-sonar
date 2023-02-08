db3d772384378aa34f6bbd3354e96a7449299412
Евгений Степанов <evgeniy.stepanov@ru.axept.com>
2023-02-08 14:25:49 +0300

    Fix indicator model

--- src/console_api/indicator/models.py
+++ src/console_api/indicator/models.py
@@ -168,7 +168,7 @@ class Indicator(models.Model):
         return (
             {
                 "type": activity.activity_type,
-                "details": [tag.id for tag in self.tags_ids],
+                "details": [tag.get("id") for tag in self.tags_ids],
                 "created-at": activity.created_at,
             }
             for activity in IndicatorActivities.objects.filter(

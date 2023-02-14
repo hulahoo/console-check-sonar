25833d1a67d4a0c0c1cae28d0165aa01471ebff5
Евгений Степанов <evgeniy.stepanov@ru.axept.com>
2023-02-14 13:58:20 +0300

    Add endpoint for change user data

--- src/console_api/users/urls.py
+++ src/console_api/users/urls.py
@@ -2,10 +2,11 @@
 
 from django.urls import path
 
-from console_api.users.views import UserView, UserDetail
+from console_api.users.views import UserView, UserDetail, UserChangePasswordView
 
 
 urlpatterns = [
     path("", UserDetail.as_view(), name="user-create-get"),
     path("/<int:user_id>", UserView.as_view()),
+    path("/change-password/<int:user_id>", UserChangePasswordView.as_view()),
 ]

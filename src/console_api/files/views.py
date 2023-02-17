953e3d2d385b778b379729fe27f32c5610f5190b
Stanislav Larionov <stanislav.larionov@ru.axept.com>
2023-02-17 12:12:13 +0300

    Rollback authentication for files api

--- src/console_api/files/views.py
+++ src/console_api/files/views.py
@@ -3,15 +3,21 @@ from django.http import HttpResponse
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.status import HTTP_406_NOT_ACCEPTABLE, HTTP_201_CREATED
 from rest_framework.parsers import MultiPartParser, FormParser
 
 from console_api.files.forms import UploadFileForm
 from console_api.files.models import Files
 from console_api.files.services import update_key_with_ts, get_hash
+from console_api.services import (
+    CustomTokenAuthentication,
+)
 
 
 class FilesView(APIView):
+    authentication_classes = [CustomTokenAuthentication]
+    permission_classes = [IsAuthenticated]
     parser_classes = (MultiPartParser, FormParser)
 
     def get(self, request: Request, *args, **kwargs) -> Response:
